f6273cea57c60d07345991e34b8600a1d2707a97
smarthimawari <yu.hsuan.huang.36@gmail.com>
2018-11-15 14:25:22 +0800

    add num select

--- guessnumber.py
+++ guessnumber.py
@@ -1,6 +1,16 @@
 import random
 
-r = random.randint(0, 100) 
+while True: 
+	a = input('Enter one number:') 
+	a = int(a)
+	b = input('Enter a second number:') 
+	b = int(b)
+	if a > b:
+		print('Wrong, the seonnd number must be larger than the first one.')
+	else: 
+		break
+
+r = random.randint(a, b)
 
 count = 0 #讓使用者可以計數，需要放在迴圈外，因為放迴圈內會歸零
 while True:
@@ -8,7 +18,7 @@ while True:
 	guess = input('Guess a number:')
 	guess = int(guess) 
 	if guess == r:
-		print('You are right!', 'Times tried:', count)  
+		print('You are right!', 'Times tried:', count)   
 		break
 	elif guess > r:
 		print('Your guess is > r.')
